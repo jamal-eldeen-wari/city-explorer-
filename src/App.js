5b675bf975d5a540233bade2eac32f333e3df92e
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-08-04 15:10:16 +0300

    fixing problems in my code

--- src/App.js
+++ src/App.js
@@ -24,29 +24,21 @@ export class App extends Component {
       `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${locationName}&format=json`
     );
     const locationIQData = result.data[0];
-    const cityName= locationIQData.display_name.split(',')[0];
-    const forecast = await axios.get(`${process.env.REACT_APP_SERVER_URL}/weather?searchQuery=${cityName}&lat=${locationIQData.lat}&lon=${locationIQData.lon}`)
+    const forecast = await axios.get(`${process.env.REACT_APP_SERVER_URL}/weather-bit?latitude=${locationIQData.lat}&longitude=${locationIQData.lon}`)
     try{
       this.setState({
-        data: locationIQData.data,
-        forcastData:forecast,
+        data: locationIQData,
+        forecastData:forecast.data,
         viewMap: true
       });
 
     }catch{
       this.setState({
         errorMesage: true
-        // mapData:map.mapData
-        // mapData:map.mapData[0]
       });
 
     }
-    // let latData = this.state.data.lat;
-    // let lonData = this.state.data.lon;
-    // console.log(this.state.data.lat);
-    // const map = await axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${latData},${lonData}&zoom=1-18`);
     console.log(result.data[0]);
-    // console.log(map)
   };
   render() {
     return (
@@ -62,14 +54,6 @@ export class App extends Component {
           ></input>
           <input type="submit" placeholder="Explore"></input>
         </form>
-        {/* <div>
-          <h4>Location Information:</h4>
-          {this.state.data.lon && <p>Longitude: {this.state.data.lon}</p>}
-          {this.state.data.lat && <p>Latitude: {this.state.data.lat}</p>}
-          {this.state.data.display_name && (
-            <p>City Name: {this.state.data.display_name}</p>
-          )}
-        </div> */}
 
         <ListGroup>
           <ListGroup.Item variant="primary">
@@ -84,7 +68,6 @@ export class App extends Component {
           )}
           </ListGroup.Item>
         </ListGroup>
-        {/* <img src = {`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.data.lat},${this.state.data.lon}&zoom=1-18`} alt = 'map'/> */}
 
         { this.state.viewMap &&
         <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.data.lat},${this.state.data.lon}&zoom=1-18`} fluid />
@@ -94,12 +77,12 @@ export class App extends Component {
           this.state.message
         }
 
-        {this.state.forcastData &&
+        {this.state.forecastData &&
         this.state.forecastData.map(weather =>{
           return(
             <section>
               <p>
-                {weather.datetime}
+                {weather.date}
 
               </p>
               <p>
